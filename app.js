f2b73473ffad96148520e29ebdcf21c349fe4587
Olabusiyi <olabusiyi@gmail.com>
2022-09-03 02:23:31 -0500

    Add gitignore, procfile and update ports

--- app.js
+++ app.js
@@ -66,19 +66,6 @@ app.get("/posts/:postId", function(req,res){
     })
   })
 
-//   let requestedTitle = _.lowerCase(req.params.postName);
-
-
-// posts.forEach(function(post){
-//   let storedTitle = _.lowerCase(post.title);
-
-
-//   if(requestedTitle === storedTitle){
- 
-
-//   } 
-// })
-
 
 })
 
@@ -99,7 +86,13 @@ app.post("/compose", function(req,res){
   
 })
 
+let port = process.env.PORT;
+if (port == null || port == "") {
+  port = 3000;
+}
+app.listen(port);
 
-app.listen(3000, function() {
-  console.log("Server started on port 3000");
+
+app.listen(port, function() {
+  console.log("Server has started successfully");
 });
